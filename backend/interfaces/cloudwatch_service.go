3b94bc61099733b74cac4bc0870c43edc286044f
Pavan Kulkarni <pavankulkarni369@gmail.com>
2026-07-30 06:18:52 +0530

    refactor: remove unused CloudWatch, Health, and Airflow service interface definitions

--- backend/interfaces/cloudwatch_service.go
+++
@@ -1,3 +0,0 @@
-// Package interfaces defines service layer contracts.
-// CloudWatch integration is out of scope for the MVP.
-package interfaces
--- backend/interfaces/health_service.go
+++
@@ -1,9 +0,0 @@
-package interfaces
-
-import "github.com/openclimatefix/hexatron/backend/structures/responses"
-
-// HealthService defines the contract for the health check service.
-type HealthService interface {
-	// GetHealth returns the current operational health of the Hexatron API.
-	GetHealth() responses.HealthResponse
-}
